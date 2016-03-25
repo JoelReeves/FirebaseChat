5ce80e79b5e25e9791ec426431fe5f13e4a25a09
Joel Reeves <jreeves@tonicdesign.com>
2016-03-25 12:30:38 -0400

    added criteria for creating an account

--- app/src/main/java/com/bromancelabs/firebasechat/activities/CreateAccountActivity.java
+++ app/src/main/java/com/bromancelabs/firebasechat/activities/CreateAccountActivity.java
@@ -26,6 +26,10 @@ public class CreateAccountActivity extends AppCompatActivity {
     @Bind(R.id.txt_login_password) EditText passwordEditText;
     @Bind(R.id.txt_confirm_password) EditText confirmPasswordEditText;
 
+    private String username;
+    private String password;
+    private String confirmPassword;
+
     private ProgressDialog progressDialog;
 
     @Override
@@ -38,10 +42,11 @@ public class CreateAccountActivity extends AppCompatActivity {
 
     @OnClick(R.id.btn_sign_up)
     public void signUpClick() {
-        String username = usernameEditText.getText().toString();
-        String password = passwordEditText.getText().toString();
+        username = usernameEditText.getText().toString();
+        password = passwordEditText.getText().toString();
+        confirmPassword = confirmPasswordEditText.getText().toString();
 
-        if (!TextUtils.isEmpty(username) && !TextUtils.isEmpty(password)) {
+        if (isAccountCriteriaSatisfied()) {
             progressDialog = new ProgressDialog(this);
             progressDialog.show();
 
@@ -50,7 +55,7 @@ public class CreateAccountActivity extends AppCompatActivity {
                 public void onSuccess(Map<String, Object> stringObjectMap) {
                     cancelProgressDialog();
                     Timber.d("Successfully created user account with uid: %s", stringObjectMap.get("uid"));
-                    Snackbar.make(usernameEditText, "Account successfully created", Snackbar.LENGTH_SHORT).show();
+                    showSnackbar(R.string.create_account_successful);
                     finish();
                     startActivity(new Intent(CreateAccountActivity.this, LoginActivity.class));
                 }
@@ -59,17 +64,38 @@ public class CreateAccountActivity extends AppCompatActivity {
                 public void onError(FirebaseError firebaseError) {
                     cancelProgressDialog();
                     Timber.e(firebaseError.getMessage());
-                    Snackbar.make(usernameEditText, "Error creating account", Snackbar.LENGTH_LONG).show();
+                    showSnackbar(R.string.create_account_error);
                 }
             });
-        } else {
-            Snackbar.make(usernameEditText, "Please fill out both fields", Snackbar.LENGTH_SHORT).show();
         }
+    }
+
+    private boolean isAccountCriteriaSatisfied() {
+        int errorCount = 0;
+
+        if (TextUtils.isEmpty(username) || TextUtils.isEmpty(password) || TextUtils.isEmpty(confirmPassword)) {
+            errorCount++;
+            showSnackbar(R.string.create_account_missing_fields);
+        }
+
+
+        if (!password.equals(confirmPassword)) {
+            errorCount++;
+            showSnackbar(R.string.create_account_password_mismatch);
+        }
+
+        Timber.d("Error count: %d", errorCount);
+
+        return errorCount == 0;
     }
 
     private void cancelProgressDialog() {
         if (progressDialog != null) {
             progressDialog.cancel();
         }
+    }
+
+    private void showSnackbar(int message) {
+        Snackbar.make(usernameEditText, message, Snackbar.LENGTH_SHORT).show();
     }
 }
