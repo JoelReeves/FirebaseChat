d070a72527e3ea3ae2c18c2f21aff83577898991
Joel Reeves <jreeves@tonicdesign.com>
2016-03-27 08:14:12 -0400

    creating utility class for Dialogs

---
+++ app/src/main/java/com/bromancelabs/firebasechat/utils/DialogUtils.java
@@ -0,0 +1,26 @@
+package com.bromancelabs.firebasechat.utils;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
+
+public class DialogUtils {
+
+    public static Dialog showProgressDialog(@NonNull Activity activity) {
+        if (activity.isFinishing()) {
+            return null;
+        }
+
+        Dialog progressDialog = new ProgressDialog(activity);
+        progressDialog.show();
+
+        return progressDialog;
+    }
+
+    public static void cancelProgressDialog(Dialog dialog) {
+        if (dialog != null) {
+            dialog.cancel();
+        }
+    }
+}
